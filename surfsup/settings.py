974ae874ae82a95eecbe142458e43376f7ef41ef
Leo Cucinell <leocucinell@gmail.com>
2022-02-01 12:31:08 -1000

    added base application pages

--- surfsup/settings.py
+++ surfsup/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps
+    'base',
+    #3rd party
 ]
 
 MIDDLEWARE = [
