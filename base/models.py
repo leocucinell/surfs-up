6707a009fbbeb79d4ff56266e4d868075fe29b08
Leo Cucinell <leocucinell@gmail.com>
2022-02-02 18:00:59 -1000

    made model attribute classes

--- base/models.py
+++ base/models.py
@@ -1,18 +1,29 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Island(models.Model):
-    name = ''
-    beaches = ''
+    name = models.CharField(max_length=350)
+    #beaches = ''
 
 class Beach(models.Model):
-    name = ''
-    island = ''
-    difficulty = ''
-    posts = ''
+    name = models.CharField(max_length=350)
+    island = models.ForeignKey(
+        'Island',
+        on_delete=models.CASCADE,
+    )
+    difficulty = models.BigIntegerField()
+    #posts = ''
 
-class Post(model):
-    title = ''
-    body = ''
-    image = ''
-    author = ''
+class Post(models.Model):
+    title = models.CharField(max_length=350)
+    body = models.TextField()
+    image = models.FileField(blank=True, null=True) #TODO: Decide where to upload this later
+    beach = models.ForeignKey(
+        'Beach',
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
